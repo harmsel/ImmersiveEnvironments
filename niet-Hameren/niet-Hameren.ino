24b06b4ed68efe68af70c6d5e932837336bd0bb8
Harm van Vugt <h.e.m.van.vugt@hva.nl>
2021-04-02 11:54:31 +0200

    Zonder Delay - Nog testen en debuggen
    Code bevat een mix van:
    - eenmalig starten bij verandering
    - starten om de zoveel tijd (zonder delay)

--- niet-Hameren/niet-Hameren.ino
+++ niet-Hameren/niet-Hameren.ino
@@ -10,8 +10,6 @@ int oudeSensorWaarde;
 //50K - 50M tussen de pinnen. weerstand. Hoe groter des te gevoeliger
 CapacitiveSensor   sensCap = CapacitiveSensor(11, 13);       // pin 13 is de sensor
 
-
-
 void setup()
 {
   //sensCap.set_CS_AutocaL_Millis(0xFFFFFFFF);     // zet autocalibrate uit (elke 20 seconden)
@@ -23,25 +21,23 @@ void loop()
   long start = millis();
   long sensorWaarde =  sensCap.capacitiveSensor(30);
 
- // Serial.print(millis() - start);        // performance in milliseconds
+  // Serial.print(millis() - start);        // performance in milliseconds
   Serial.println(sensorWaarde);                  //  sensor output
 
-if (sensorWaarde != oudeSensorWaarde){
-  Serial.println("verschil in waarde");
+  if (sensorWaarde != oudeSensorWaarde) {
+    Serial.println("verschil in waarde");
   }
 
   oudeSensorWaarde = sensorWaarde;
 
-  if (sensorWaarde > 1000) { //gevoeligheid
-    noteOn(0, 48, 64);   // Channel 0, middle C, normal velocity
-    MidiUSB.flush();
+  if (sensorWaarde > 1000 && (wachtFF1 > wachtTijd)) { //gevoeligheid
     if (wachtFF1 > wachtTijd) {
       wachtFF1 = 0; //de wekker weer op 0 zetten
-      noteOff(0, 48, 64); 
+      noteOn(0, 48, 64);   // Channel 0, middle C, normal velocity
       MidiUSB.flush();
     }
 
-  } else if (sensorWaarde > 600) {//gevoeligheid
+  } else if (sensorWaarde != oudeSensorWaarde) {//gevoeligheid
     Serial.println("S3");
     noteOn(0, 60, 64);   // Channel 0, C3, normal velocity
     MidiUSB.flush();
@@ -58,7 +54,7 @@ if (sensorWaarde != oudeSensorWaarde){
     MidiUSB.flush();
     if (wachtFF3 > wachtTijd) {
       wachtFF3 = 0; //de wekker weer op 0 zetten
-      noteOff(0, 63, 64);  
+      noteOff(0, 63, 64);
       MidiUSB.flush();
     }
   }
@@ -68,7 +64,7 @@ if (sensorWaarde != oudeSensorWaarde){
 }
 
 
-  
+
 void noteOn(byte channel, byte pitch, byte velocity) {
   midiEventPacket_t noteOn = {0x09, 0x90 | channel, pitch, velocity};
   MidiUSB.sendMIDI(noteOn);
