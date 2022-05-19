846ab3c437e759b4726f3e10c332ab56f6fd82a8
Harm van Vugt <h.e.m.van.vugt@hva.nl>
2022-05-19 12:34:10 +0200

    update

--- MIDI-afstand_difference-bo/MIDI-afstand_difference-bo.ino
+++ MIDI-afstand_difference-bo/MIDI-afstand_difference-bo.ino
@@ -5,9 +5,9 @@
 Ultrasonic ultrasonic(4);//dus op D4
 
 
-int afstandOld; //Nodig
-int offset = 5; //offset tussen de bewegingen CM
-int vol = 0;
+int afstandOld; // -- Nodig
+int offset = 5; // --- offset tussen de bewegingen CM
+int vol = 0; // --- begin vol
 
 void setup() {
   Serial.begin(115200);
@@ -21,7 +21,7 @@ void loop() {
   int stand = afstand;
   Serial.println(vol);
 
-  // --- DIT DEEEL IS VOLUME AANPASSEN DOOR DANSEN
+  // --- DIT DEEL IS VOLUME AANPASSEN DOOR DANSEN
   if ((afstand > afstandOld + offset || afstand < afstandOld - offset) && vol < 150) {
     Serial.println("Harde dansers in de House!!");
     vol = vol + 5; //hoe veel beloning krijgen de dansers voor beweging
@@ -33,7 +33,7 @@ void loop() {
     vol = vol - 1; // hoe snel verliezen ze muziek door niet te dansen
 
   }
-  // EINDE einde volume aan passen
+  // ---EINDE einde volume aan passen
 
 
 
