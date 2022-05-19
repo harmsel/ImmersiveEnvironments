dfe6eee833d84994b82718db3434a6e63d09edd3
Harm van Vugt <h.e.m.van.vugt@hva.nl>
2022-05-19 12:31:30 +0200

    dance DANCE voor bo

--- MIDI-afstand_difference-bo/MIDI-afstand_difference-bo.ino
+++ MIDI-afstand_difference-bo/MIDI-afstand_difference-bo.ino
@@ -7,7 +7,7 @@ Ultrasonic ultrasonic(4);//dus op D4
 
 int afstandOld; //Nodig
 int offset = 5; //offset tussen de bewegingen CM
-int vol=0;
+int vol = 0;
 
 void setup() {
   Serial.begin(115200);
@@ -17,23 +17,25 @@ void setup() {
 void loop() {
   long afstand;
   afstand = ultrasonic.MeasureInCentimeters();
- // Serial.print(afstand); Serial.println(" cm");
+  // Serial.print(afstand); Serial.println(" cm");
   int stand = afstand;
-    Serial.println(vol);
+  Serial.println(vol);
 
-  if ((afstand > afstandOld + offset || afstand < afstandOld - offset) && vol <150) {
+  // --- DIT DEEEL IS VOLUME AANPASSEN DOOR DANSEN
+  if ((afstand > afstandOld + offset || afstand < afstandOld - offset) && vol < 150) {
     Serial.println("Harde dansers in de House!!");
-    vol=vol+5;
+    vol = vol + 5; //hoe veel beloning krijgen de dansers voor beweging
 
-
-    
     afstandOld = afstand;
     delay(20); //tussen metingen
-  } else if (vol >0){
-    
-    vol=vol-1;
-    
-    }
+  } else if (vol > 0) {
+
+    vol = vol - 1; // hoe snel verliezen ze muziek door niet te dansen
+
+  }
+  // EINDE einde volume aan passen
+
+
 
   delay(30); //onthaasten
 }
