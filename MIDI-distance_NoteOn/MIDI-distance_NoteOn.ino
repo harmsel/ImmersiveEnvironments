847ef6472ee3aad527efbd68eec28c1477ec5c71
Harm van Vugt <h.e.m.van.vugt@hva.nl>
2022-09-23 20:51:21 +0200

    works

--- MIDI-distance_NoteOn/MIDI-distance_NoteOn.ino
+++ MIDI-distance_NoteOn/MIDI-distance_NoteOn.ino
@@ -1,9 +1,8 @@
-
 #include "MIDIUSB.h"
 #include "Ultrasonic.h"
 
-Ultrasonic ultrasonic(13);//plug in 7
-int stopHammer=999;
+Ultrasonic ultrasonic(13);  //plug in 4
+int stopHammer = 999;
 
 void setup() {
   Serial.begin(115200);
@@ -14,41 +13,44 @@ void loop() {
   distance = ultrasonic.MeasureInCentimeters();
   Serial.print(distance); Serial.println(" cm");
 
-  /// ---  if  distance is 2 cm, send note
-  if (distance == 2 && stopHammer != 1 ) {
-    Serial.println("note E2 aan");
+  /// ---  if  distance is 2 to 10 cm, send note E2. Just one time (stopHammer)
+  if (distance > 2 && distance < 10 && stopHammer != 1) {
+    Serial.println("1. ---- note E2 aan");
     noteOn(0, 48, 64);
     MidiUSB.flush();
-    stopHammer = 1;     
-  }
-  else if (distance < 5 && stopHammer != 2) {
+    stopHammer = 1;
+    // pre
+  } else if (distance > 10 && distance < 15 && stopHammer != 2) {
     Serial.println("note D2 aan");
     noteOn(0, 50, 64);
     MidiUSB.flush();
-       stopHammer = 2;  
-  }
-  else if (distance < 10 && stopHammer != 3) {
-    Serial.println("note C2 aan");
+    stopHammer = 2;
+  } else if (distance > 15 && stopHammer != 3) {
+    Serial.println(" -- note C2 aan");
     noteOn(0, 52, 64);
     MidiUSB.flush();
-stopHammer = 3;  
-  }  else{
-    stopHammer = 99;   }
- 
-  delay(1); //onthaasten
+    stopHammer = 3;
+  }
+ // Serial.println(stopHammer);
+  delay(50);  //onthaasten
 }
 
+
+
+
+/// --- Below the functions to call in the loop -- 
+// -----------------------------------------------
 void noteOn(byte channel, byte pitch, byte velocity) {
-  midiEventPacket_t noteOn = {0x09, 0x90 | channel, pitch, velocity};
+  midiEventPacket_t noteOn = { 0x09, 0x90 | channel, pitch, velocity };
   MidiUSB.sendMIDI(noteOn);
 }
 
 void noteOff(byte channel, byte pitch, byte velocity) {
-  midiEventPacket_t noteOff = {0x08, 0x80 | channel, pitch, velocity};
+  midiEventPacket_t noteOff = { 0x08, 0x80 | channel, pitch, velocity };
   MidiUSB.sendMIDI(noteOff);
 }
 
 void controlChange(byte channel, byte control, byte value) {
-  midiEventPacket_t event = {0x0B, 0xB0 | channel, control, value};
+  midiEventPacket_t event = { 0x0B, 0xB0 | channel, control, value };
   MidiUSB.sendMIDI(event);
 }
